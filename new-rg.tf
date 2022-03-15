388fae70de727d9f209317ed4e191d3db745dab6
deepak9bisht <65491975+deepak9bisht@users.noreply.github.com>
2022-03-15 23:18:39 +0000

    Update new-rg.tf

--- new-rg.tf
+++ new-rg.tf
@@ -6,7 +6,3 @@ resource "azurerm_resource_group" "example" {
   name     = "tfcloud-rg"
   location = "West Europe"
 }
- resource "azurerm_resource_group" "example1" {
-  name     = "tfcloud-new-rg"
-  location = "eastus"
-}
