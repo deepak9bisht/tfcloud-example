65e20a889fe65ced805030a20345522e917cacba
Deepak <deepak9bisht@gmail.com>
2022-03-16 00:40:42 +0000

    update

--- new-rg.tf
+++ new-rg.tf
@@ -1,12 +1,16 @@
+## CONFIGURES THE MS AZURE PROVIDER
+
 provider "azurerm" {
   features {}
 }
+
+## CREATES A RESOURCE GROUP
 
 resource "azurerm_resource_group" "example" {
   name     = "tfcloud-rg"
   location = "West Europe"
 }
-# resource "azurerm_resource_group" "example1" {
-#   name     = "tfcloud-rg-new"
-#   location = "eastus"
-# }
+resource "azurerm_resource_group" "example1" {
+  name     = "tfcloud-rg-new"
+  location = "eastus"
+}
