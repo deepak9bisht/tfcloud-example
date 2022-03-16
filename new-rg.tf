8bb630a1a84eaf7ceeaab005fa1a3bc10e9c4fdf
Deepak <deepak9bisht@gmail.com>
2022-03-16 01:06:23 +0000

    update

--- new-rg.tf
+++ new-rg.tf
@@ -9,7 +9,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "example" {
   name     = "tfcloud-rg"
   location = "West Europe"
-# }
+}
 # resource "azurerm_resource_group" "example1" {
 #   name     = "tfcloud-rg-new"
 #   location = "eastus"
